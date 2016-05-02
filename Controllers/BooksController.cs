b1c655c074a16458fb40d38490c5ab23de752051
alexkmbk <alexkmbkdr1@gmail.com>
2016-05-02 23:35:31 +0600

    Логика функции RenderPartialViewToString из контроллеров вынесена в глобальный, статический класс

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -32,24 +32,9 @@ namespace Books.Controllers
         // это позволяет передавать готовый html код в ответ на ajax запросы с клиента
         public string RenderPartialViewToString(string viewName, object model)
         {
-            if (string.IsNullOrEmpty(viewName))
-                viewName = ActionContext.ActionDescriptor.Name;
-
-            ViewData.Model = model;
-
-            using (StringWriter sw = new StringWriter())
-            {
-                var engine = Resolver.GetService(typeof(ICompositeViewEngine)) as ICompositeViewEngine;
-                ViewEngineResult viewResult = engine.FindPartialView(ActionContext, viewName);
-
-                ViewContext viewContext = new ViewContext(ActionContext, viewResult.View, ViewData, TempData, sw, new HtmlHelperOptions());
-
-                var t = viewResult.View.RenderAsync(viewContext);
-                t.Wait();
-
-                return sw.GetStringBuilder().ToString();
-            }
+            return Global.RenderPartialViewToString(this, viewName, model);
         }
+
 
         // GET: /<controller>/
         public IActionResult Index(int AuthorId, int PublisherId, bool ajax = false)
---
+++ Global.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.AspNet.Mvc.ViewEngines;
+using Microsoft.AspNet.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books
+{
+    static public class Global
+    {
+        // Функция для формирования html кода по переданному шаблону вида и модели
+        // результат возвращается в виде строки
+        // это позволяет передавать готовый html код в ответ на ajax запросы с клиента
+        static public string RenderPartialViewToString(Controller controller, string viewName, object model)
+        {
+            if (string.IsNullOrEmpty(viewName))
+                viewName = controller.ActionContext.ActionDescriptor.Name;
+
+            controller.ViewData.Model = model;
+
+            using (StringWriter sw = new StringWriter())
+            {
+                var engine = controller.Resolver.GetService(typeof(ICompositeViewEngine)) as ICompositeViewEngine;
+                ViewEngineResult viewResult = engine.FindPartialView(controller.ActionContext, viewName);
+
+                ViewContext viewContext = new ViewContext(controller.ActionContext, viewResult.View, controller.ViewData, controller.TempData, sw, new HtmlHelperOptions());
+
+                var t = viewResult.View.RenderAsync(viewContext);
+                t.Wait();
+
+                return sw.GetStringBuilder().ToString();
+            }
+        }
+    }
+}
